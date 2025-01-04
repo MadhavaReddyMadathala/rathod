a236a21d3d056a4deef267677baeb2228d510a55
Madhava Reddy <92641734+MadhavaReddyMadathala@users.noreply.github.com>
2025-01-04 16:40:45 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,33 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/Unsplash.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Unsplash";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a versatile computer engineer with expertise in full-stack development,Generative AI, and Android applications, combined with strong leadership, creativity, and a passion for solving real-world problems through technology.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web development",
+  "Mobile app development",
+  "Generative AI",
+  "Effective Communication",
+  "Leadership and Team Management",
+  "Problem-Solving and Crtical Thinking",
+  "Project Scheduling and Execution",
+  "Creative and Innovative Thinking",
+  "Collaboration and Networking",
+  "Graphic Design and Digital art",
+  "Singing,Dancing and Acting",
+  "Linguistic Proficiency(Multilingual Communication)",
 ];
 
 /**
@@ -45,8 +51,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
-
+  "Technology is my canvas, and solving problems is my art—crafting solutions for a better tomorrow.";
 const About = () => {
   return (
     <section className="padding" id="about">
